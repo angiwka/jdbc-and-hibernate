e85403c87f0c9df1704e9e177b4b6c26f25cbfd1
Lenovogit1 <gilewskanna@gmail.com>
2023-03-04 13:33:34 +0100

    [change] - interface with implementation - polymorphism

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -2,6 +2,13 @@ package pl.sda.orange2.lambda;
 
 public class LambdaExercises {
     public static void main(String[] args) {
+        BoysDontCry boy = new ChłopakiNiePłaczą();
+        boy.silnoreki();
+        // boy.sing();
 
+        ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
+        boy2.sing();
+
+        //rzutowanie ((ChłopakiNiePłaczą) boy).sing();
     }
 }
