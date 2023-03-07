f3117bef7d447c3e43f5989d41bbdccfc2a67df8
Lenovogit1 <gilewskanna@gmail.com>
2023-03-07 20:25:45 +0100

    [fix] - closing database connection

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -2,10 +2,7 @@ package pl.sda.orange2.jdbc;
 
 import pl.sda.orange2.jdbc.config.H2Config;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestConnection {
     public static void main(String[] args) {
@@ -13,29 +10,53 @@ public class TestConnection {
         System.out.println("Current class name is: " + className);
 
         String query = """
-                SELECT ID,NAME 
+                SELECT ID, NAME
                 FROM TEST
                 """;
+
+        ResultSet queryResult = null;
         try {
+//            Connection h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD);
             // we need jdbc driver (h2 dependency at pom)
-            // connection gave us access to existing database
+            // connection gave us access to existing db
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
             System.out.println("got connection: " + (h2Connection != null));
 
-            //Statement is used to send queries to db (database) with existing connection
+            // Statement is used to send queries to db with existing connection
             Statement queryStatement = h2Connection.createStatement();
+
             // ResultSet contains query result data as simple table
             // we need to iterate over result to got data
-            ResultSet queryResult = queryStatement.executeQuery(query);
-            while(queryResult.next()) {
+            queryResult = queryStatement.executeQuery(query);
+            while (queryResult.next()) {
                 System.out.println("id: " + queryResult.getInt(1));
                 System.out.println("name: " + queryResult.getString(2));
             }
         } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
             exc.printStackTrace();
+        } finally {
+            System.out.println("Finally I know it :)");
+            if (queryResult != null) {
+                try {
+                    queryResult.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
+
+        System.out.println("The end");
     }
 }
+
+
+
+
+
+
+
